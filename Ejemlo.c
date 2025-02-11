b96ab835e743390e9e65bfc5a9326fca0a943207
julss642 <leydi.solorza-g@mail.escuelaing.edu.co>
2025-02-11 14:14:42 -0500

    Refactor Ejemlo.c to remove duplicate output and streamline user input handling

--- Ejemlo.c
+++ Ejemlo.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 int main(int argc, char** argv ){
-printf("Hello World.\n" );
-printf("Bonjour.\n" );
-return 0;
-}
 char a;
 std::cin >> a;
+std::cout << "Hello World." << std::endl;
+std::cout << a << std::endl;
 
-
-std :: cout << "Hello World." << std::endl;
-std :: cout << a << std::endl;
+return 0;
+}

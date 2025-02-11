cc83eedc5a3d69ef0964fda87cd0c5370841bb59
julss642 <leydi.solorza-g@mail.escuelaing.edu.co>
2025-02-11 14:17:20 -0500

    Update Ejemlo.c to use printf for output and add French greeting

--- Ejemlo.c
+++ Ejemlo.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main(int argc, char** argv ){
-char a;
-std::cin >> a;
-std::cout << "Hello World." << std::endl;
-std::cout << a << std::endl;
-
+printf("Hello World.\n" );
+printf("Bonjour.\n" );
 return 0;
 }
---
+++ Ejemplo++.c++
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+int main(){
+char a;
+std::cin >> a;
+std::cout << "Hello World." << std::endl;
+std::cout << a << std::endl;
+
+return 0;
+}

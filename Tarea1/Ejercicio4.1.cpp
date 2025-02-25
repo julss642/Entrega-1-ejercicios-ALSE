b5f144c034764089f31c99eec24e956dd8e11369
julss642 <leydi.solorza-g@mail.escuelaing.edu.co>
2025-02-25 13:43:41 -0500

    Add Ejercicio4.1.cpp and implement even/positive number check in Ejercicio2.2.cpp; remove old Ejercicio2.2.cpp

---
+++ Tarea1/Ejercicio4.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main() {
+    int a[5];
+    std::cout << "Ingrese un número entero: ";
+    for (int i = 0; i < 5; i++){
+        std::cin >> a[i];
+    }
+    int suma = 0;
+    for (int i = 0; i < 5; i++){
+        suma += a[i];
+    }
+    bool esPar = suma%2 == 0;
+    bool esPositivo = suma > 0;
+
+    std::cout << "La suma de los números ingresados es: " << suma << std::endl;
+    std::cout << (esPar? "Es par" : "No es par") << std::endl;
+    std::cout << (esPositivo? "Es positivo" : "No es positivo") << std::endl;
+    return 0;
+}
